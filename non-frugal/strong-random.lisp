d4ecd24029ffad835347bde17864216467681318
Ákos Kiss <ak@coram.pub>
2023-07-29 09:48:45 +0200

    Update obsolete comment

--- non-frugal/strong-random.lisp
+++ non-frugal/strong-random.lisp
@@ -6,6 +6,7 @@
 (initialize-random #'crypto:strong-random
                    (lambda () (ironclad:make-prng :os)))
 
-;; Clear version 1 generator (will be initialized on first use)
+;; Clear version 1 & 7 generators (they will be initialized on first
+;; use):
 (setf *v1-generator* nil
       *v7-generator* nil)
