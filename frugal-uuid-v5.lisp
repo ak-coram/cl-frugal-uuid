943771bc185daef4740f909aeb4be2a7405af332
kilianmh <kilian.haemmerle@protonmail.com>
2024-07-12 00:48:56 +0200

    Fix: function return type declaration
    Add `(values ... &optional)` in function return type
    declarations wherever this was not present yet.

--- frugal-uuid-v5.lisp
+++ frugal-uuid-v5.lisp
@@ -2,14 +2,14 @@
 
 (in-package #:frugal-uuid)
 
-(declaim (ftype (function (t) uuid) make-v5-from-integer))
+(declaim (ftype (function (t) (values uuid &optional)) make-v5-from-integer))
 (defun make-v5-from-integer (i)
   "Set the bits for version 5 and IETF variant, return uuid value."
   (setf (ldb (byte 4 76) i) #x5      ; Set version to 5
         (ldb (byte 2 62) i) #b10)    ; Set variant to IETF
   (from-integer i))
 
-(declaim (ftype (function ((simple-array (unsigned-byte 8))) uuid)
+(declaim (ftype (function ((simple-array (unsigned-byte 8))) (values uuid &optional))
                 make-v5-from-octets))
 (defun make-v5-from-octets (octets)
   "Set the bits for version 5 and IETF variant, return uuid value."
